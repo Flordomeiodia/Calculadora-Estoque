89e6054c2ea107cf498d99218132f100228db4a8
Ana Maria <mendesanamaria05@gmail.com>
2024-08-27 21:20:10 -0300

    somar.py

---
+++ 1 -somar.py
@@ -0,0 +1,8 @@
+#fazendo conta de somar
+#Na teoria é Num1 + Num2 = resultado
+def soma():
+    X = float(input("Digite o primeiro número da conta:"))
+    Y = float(input("Digite o segundo número da conta:"))
+    print("Soma: ", X+Y)
+
+
