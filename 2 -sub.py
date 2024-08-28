dca85c905b7e708da3fd8593aab68d36dd721497
Ana Maria <mendesanamaria05@gmail.com>
2024-08-27 21:25:47 -0300

    sub.py

---
+++ 2 -sub.py
@@ -0,0 +1,6 @@
+#fazendo conta de subtração
+#Na teoria é Num1 - Num2 = resultado
+def soma():
+    X = float(input("Digite o primeiro número da conta:"))
+    Y = float(input("Digite o segundo número da conta:"))
+    print("O Resultado da sua subtração é : ", X-Y)
