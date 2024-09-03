1c51c256d750cd3250ddae7429de88984acaee3b
Aline Albanez <aline.goncalves.faria@gmail.com>
2024-09-03 20:15:12 -0300

    Modifica função calculadora.

--- 2 -sub.py
+++ 2 -sub.py
@@ -1,7 +1,7 @@
 #fazendo conta de subtração
 #Na teoria é Num1 - Num2 = resultado
 
-def Subtração():
+def Subtracao():
     X = float(input("Digite o primeiro número da conta:"))
     Y = float(input("Digite o segundo número da conta:"))
     resultado_Subtração: X - Y  # type: ignore
