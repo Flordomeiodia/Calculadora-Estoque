09ca86c2515119b8061e432bace7ea7a42dd8ea8
Ana Maria <mendesanamaria05@gmail.com>
2024-09-03 20:34:52 -0300

    salvar alterações

--- 1 -somar.py
+++ 1 -somar.py
@@ -3,7 +3,7 @@
 def soma():
     X = float(input("Digite o primeiro número da conta:"))
     Y = float(input("Digite o segundo número da conta:"))
-    resultado_soma: X+Y # type: ignore
+    resultado_Soma: X+Y # type: ignore
     print("Resultado da sua conta:{}".format(resultado_Soma))
 
 
